406c71592c0dd7f16b25605b577d6f24eb258b2f
NikaKvaratskhelia <nikakvaratskhelia13@gmail.com>
2025-03-01 12:09:00 +0400

    new pages added

---
+++ games.js
@@ -0,0 +1,83 @@
+document.getElementById('photo-1').addEventListener('click', () => {
+    document.getElementById('photo-1').classList.add('active');  
+});
+
+document.getElementById('extend-button1').addEventListener('click', () => {
+    document.getElementById('photo-1').classList.add('extend')
+})
+
+document.getElementById('close-button1').addEventListener('click', () => {
+    document.getElementById('photo-1').classList.remove('extend')
+    document.getElementById('photo-1').classList.remove('active');  
+});
+
+
+
+
+
+document.getElementById('photo-2').addEventListener('click', () => {
+    document.getElementById('photo-2').classList.add('active');  
+});
+
+document.getElementById('extend-button2').addEventListener('click', () => {
+    document.getElementById('photo-2').classList.add('extend')
+})
+
+document.getElementById('close-button2').addEventListener('click', () => {
+    document.getElementById('photo-2').classList.remove('extend')
+    document.getElementById('photo-2').classList.remove('active');  
+});
+
+
+
+
+document.getElementById('photo-3').addEventListener('click', () => {
+    document.getElementById('photo-3').classList.add('active');  
+});
+
+
+document.getElementById('extend-button3').addEventListener('click', () => {
+    document.getElementById('photo-3').classList.add('extend')
+})
+
+document.getElementById('close-button3').addEventListener('click', () => {
+    document.getElementById('photo-3').classList.remove('extend')
+    document.getElementById('photo-3').classList.remove('active');  
+});
+
+
+
+
+
+document.getElementById('photo-4').addEventListener('click', () => {
+    document.getElementById('photo-4').classList.add('active');  
+});
+
+
+document.getElementById('extend-button4').addEventListener('click', () => {
+    document.getElementById('photo-4').classList.add('extend')
+})
+
+
+document.getElementById('close-button4').addEventListener('click', () => {
+    document.getElementById('photo-4').classList.remove('extend')
+    document.getElementById('photo-4').classList.remove('active');  
+});
+
+
+
+
+
+document.getElementById('photo-5').addEventListener('click', () => {
+    document.getElementById('photo-5').classList.add('active');  
+});
+
+document.getElementById('extend-button5').addEventListener('click', () => {
+    document.getElementById('photo-5').classList.add('extend')
+})
+
+document.getElementById('close-button5').addEventListener('click', () => {
+    document.getElementById('photo-5').classList.remove('extend')
+    document.getElementById('photo-5').classList.remove('active');  
+});
+
